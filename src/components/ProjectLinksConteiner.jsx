e3cae2abd4a7a07cce338b7ab50b908520b1b659
Ondra Svec <ondrasvecveslo@gmail.com>
2023-08-19 09:09:17 +0200

    Add ProjectLinksContainer component

---
+++ src/components/ProjectLinksConteiner.jsx
@@ -0,0 +1,10 @@
+const ProjectLinksContainer = ({ children }) => {
+  return (
+    <div className="w-full grid grid-cols-4">
+      <div></div>
+      {children}
+    </div>
+  );
+};
+
+export default ProjectLinksContainer;
