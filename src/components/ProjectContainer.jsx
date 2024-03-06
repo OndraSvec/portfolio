5cee75eb1b289e32f82d311fed827228e24c388f
Ondra Svec <ondrasvecveslo@gmail.com>
2024-03-06 14:32:17 +0100

    Add margin bottom to projects

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,7 +1,7 @@
 const ProjectContainer = ({ flexRowReversed = false, children }) => {
   return (
     <div
-      className={`flex flex-col sm:text-lg md:text-xl justify-center items-start gap-4 md:gap-6 lg:gap-8 ${
+      className={`flex flex-col mb-10 sm:text-lg sm:mb-16 md:text-xl md:mb-32 justify-center items-start gap-4 md:gap-6 lg:gap-8 lg:mb-40 ${
         flexRowReversed ? "lg:flex-row-reverse" : "lg:flex-row"
       }`}
     >
